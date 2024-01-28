b9200f01e39f56c2d9ebffd48ab31cc40ff42c25
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-28 01:28:57 -0600

    debugging

--- SceneBase.cs
+++ SceneBase.cs
@@ -5,25 +5,39 @@ using System.Collections.Generic;
 
 public partial class SceneBase : Node3D
 {
-	const float StartTicks = 2000;
+    const float StartTicks = 2000;
 
-	const string path = "C:\\Users\\jnick\\AppData\\Roaming\\SpaceEngineers\\Saves\\76561198274566684\\Mod Profiler Test\\Storage\\SCCoordinateOutput_ScCoordWriter\\";
-	List<GridMovementData> movementDatas = new List<GridMovementData>();
-	MeshInstance3D meshInstance;
-	float tick = StartTicks;
+    const string path = "C:\\Users\\User\\AppData\\Roaming\\SpaceEngineers\\Saves\\76561198071098415\\SCTacView\\Storage\\SCCoordinateOutput_ScCoordWriter\\";
+    List<GridMovementData> movementDatas = new List<GridMovementData>();
+    MeshInstance3D meshInstance;
+    float tick = StartTicks;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		foreach (var file in DirAccess.GetFilesAt(path))
-			movementDatas.Add(new GridMovementData(path + file));
-		meshInstance = GetChild<MeshInstance3D>(0);
-		meshInstance.Scale = ((Vector3)movementDatas[0].GridBox) * movementDatas[0].GridSize;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        foreach (var file in DirAccess.GetFilesAt(path))
+        {
+            string fullPath = System.IO.Path.Combine(path, file);
+            movementDatas.Add(new GridMovementData(fullPath));
+        }
+
+        // Debug statement to confirm the list is populated
+        GD.Print("Number of movement data loaded: " + movementDatas.Count);
+
+        if (movementDatas.Count > 0)
+        {
+            meshInstance = GetChild<MeshInstance3D>(0);
+            meshInstance.Scale = ((Vector3)movementDatas[0].GridBox) * movementDatas[0].GridSize;
+        }
+        else
+        {
+            GD.Print("No movement data files found or loaded.");
+        }
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
         if (Input.IsKeyPressed(Key.R))
         {
             tick = StartTicks;
@@ -31,13 +45,20 @@ public partial class SceneBase : Node3D
                 data.Reset();
         }
 
-        tick += (float) delta;
+        tick += (float)delta;
 
-        DisplayServer.WindowSetTitle(Math.Round(tick/60) + "s | " + Engine.GetFramesPerSecond() + "fps");
+        DisplayServer.WindowSetTitle(Math.Round(tick / 60) + "s | " + Engine.GetFramesPerSecond() + "fps");
 
-        meshInstance.GlobalPosition = movementDatas[0].GetPosition(tick);
-		meshInstance.Quaternion = movementDatas[0].GetRotation(tick);
+        if (movementDatas.Count > 0)
+        {
+            meshInstance.GlobalPosition = movementDatas[0].GetPosition(tick);
+            meshInstance.Quaternion = movementDatas[0].GetRotation(tick);
+        }
+        else
+        {
+            GD.Print("No data available to update position and rotation.");
+        }
 
-		tick++;
-	}
+        tick++;
+    }
 }
